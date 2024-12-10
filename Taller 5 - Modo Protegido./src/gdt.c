5926e237d4460facce411e418e023d43bececb26
matruizz <materuizz1299@gmail.com>
2024-12-10 14:33:36 -0300

    gdt.c modificada.

--- Taller 5 - Modo Protegido./src/gdt.c
+++ Taller 5 - Modo Protegido./src/gdt.c
@@ -113,6 +113,12 @@ gdt_entry_t gdt[GDT_COUNT] = {
             .g = 0x0,
             .base_31_24 = GDT_BASE_HIGH(VIDEO),
         },
+    /* Completar la GDT: 
+      Es conveniente completar antes las constantes definidas en defines.h y valerse
+      de las mismas para definir los descriptores acá. Traten en lo posible de usar las 
+      macros allí definidas.
+      Tomen el descriptor nulo como ejemplo y definan el resto.
+    */
 };
 
 // Aca hay una inicializacion estatica de una structura que tiene su primer componente el tamano 
